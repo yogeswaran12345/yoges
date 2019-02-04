8cdec8327eea8c2c3fc8814743fce19628674af9
yogeswaran12345 <47219404+yogeswaran12345@users.noreply.github.com>
2019-02-03 22:24:44 -0800

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,7 +1,10 @@
-h=input("enter a character: ")
-if=(ch=='A'or ch=='a'or ch=='E'or ch=='e'or ch=='I'or ch=='i'orch=='O'or ch=='o'or ch=='U'or ch=='u'):
-   print(ch,"vowel")
+ch = input()
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print("Vowel")
+elif(ch=='B' or ch=='b' or ch=='C' or ch=='c' or ch=='D' or ch=='d' or ch=='F'
+or ch=='f' or ch=='G' or ch=='g'or ch=='H'or ch=='h'or ch=='J'or ch=='j'or ch=='K' or ch=='k'or ch=='L' or ch=='l'or ch=='M'or ch=='m'or ch=='N'or ch=='n'or ch=='P'or ch=='p'or ch=='Q'or ch=='q'or ch=='R'or ch=='r'or ch=='S'or ch=='s'or ch=='T'or ch=='t'or ch=='V'or ch=='v'or ch=='W'or ch=='w'or ch=='X'or ch=='x'or ch=='Y'or ch=='y'or ch=='Z'or ch=='z'):
+	print("Consonant")
 else:
-if=(ch=='B'or ch=='b' ch=='C'or ch=='c' ch=='D'or ch=='d' ch=='F'or ch=='f'ch=='G'or ch=='g' ch=='H or ch=='h')
-   print(ch, "constant")
+	print("invalid")
 	
