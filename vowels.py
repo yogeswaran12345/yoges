b72abea2e503d8ee9ac79a70d2ba493da4146361
yogeswaran12345 <47219404+yogeswaran12345@users.noreply.github.com>
2019-02-03 20:21:42 -0800

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,7 @@
+h=input("enter a character: ")
+if=(ch=='A'or ch=='a'or ch=='E'or ch=='e'or ch=='I'or ch=='i'orch=='O'or ch=='o'or ch=='U'or ch=='u'):
+   print(ch,"vowel")
+else:
+if=(ch=='B'or ch=='b' ch=='C'or ch=='c' ch=='D'or ch=='d' ch=='F'or ch=='f'ch=='G'or ch=='g' ch=='H or ch=='h')
+   print(ch, "constant")
+	
