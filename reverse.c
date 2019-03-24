b88610845c602d1fe33e254b9e784b87c229dce2
yogeswaran12345 <47219404+yogeswaran12345@users.noreply.github.com>
2019-03-24 22:10:00 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,32 +1,25 @@
-#include <stdio.h>
-#include<conio.h>
-
-int main(void)
+#include<stdio.h>
+void main()
 {
-	int a[5],i,j,temp;
-	for(i=1;i<=5;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	
-	   
-	    for(i=1;i<=5;i++)
-	   {
-	       for(j=1+i;j<5;j++)
-	       {
-	           if(a[i]<a[j])
-	           {
-	               temp=a[i];
-	               a[i]=a[j];
-	               a[j]=temp;
-	           }
-	       }
-	   }
-	   
-	   
-	   for(i=1;i<=5;i++)
-	   {
-	   printf("%d",a[i]);
-	   }
-	   
+int n,i,j,temp;
+scanf("%d",&n);
+int a[n];
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+for(j=i;j<n;j++)
+{
+if(a[j]>a[i])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
 }
